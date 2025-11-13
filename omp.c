b6008c1d842ae7a48ecdd6ebd54dacde023d0e14
AnterCreeper <wangzhihao9@hotmail.com>
2025-11-13 16:55:40 +0800

    Revert omp.c

--- omp.c
+++ omp.c
@@ -14,8 +14,6 @@
 
 #include "common.h"
 
-typedef double v4f64 __attribute__((__vector_size__(32))); //256
-
 void transpose(double* A, double* AA, int i) {
     for(int j = 0; j < 4; j++) {
         int base = i*4+j*4*32;
@@ -36,25 +34,25 @@ void transpose(double* A, double* AA, int i) {
 }
 
 static inline void matmul_worker_row(double* A, double* B, double* C, int i, int stripe) {
-    v4f64 Csum[4];
-    for(int j = 0; j < 4; j++) Csum[j] = {0, 0, 0, 0};
+    __m256d Csum[4];
+    for(int j = 0; j < 4; j++) Csum[j] = _mm256_setzero_pd();
     for(int j = 0; j < 4; j++) {
         int base = i*4+j*4*16000;
 #pragma GCC unroll 4
         for(int k = 0; k < 4; k++) {
-            v4f64 B_4 = *(v4f64*)&B[base+k*16000];
-            double A0 = A[j*128+k*32];
-            double A1 = A[j*128+k*32+1];
-            double A2 = A[j*128+k*32+2];
-            double A3 = A[j*128+k*32+3];
-            Csum[0] = {A0, A0, A0, A0} * B_4 + Csum[0];
-            Csum[1] = {A1, A1, A1, A1} * B_4 + Csum[0];
-            Csum[2] = {A2, A2, A2, A2} * B_4 + Csum[0];
-            Csum[3] = {A3, A3, A3, A3} * B_4 + Csum[0];
+            __m256d B_4 = *(__m256d*)&B[base+k*16000];
+            __m256d A1 = _mm256_broadcast_sd(&A[j*128+k*32]);
+            __m256d A2 = _mm256_broadcast_sd(&A[j*128+k*32+1]);
+            __m256d A3 = _mm256_broadcast_sd(&A[j*128+k*32+2]);
+            __m256d A4 = _mm256_broadcast_sd(&A[j*128+k*32+3]);
+            Csum[0] = _mm256_fmadd_pd(A1, B_4, Csum[0]);
+            Csum[1] = _mm256_fmadd_pd(A2, B_4, Csum[1]);
+            Csum[2] = _mm256_fmadd_pd(A3, B_4, Csum[2]);
+            Csum[3] = _mm256_fmadd_pd(A4, B_4, Csum[3]);
         }
     }
     //stream store show poor performance
-    for(int j = 0; j < 4; j++) *(v4f64*)&C[i*4+j*16000] = Csum[j];
+    for(int j = 0; j < 4; j++) _mm256_store_pd(&C[i*4+j*16000], Csum[j]);
     return;
 }
 
